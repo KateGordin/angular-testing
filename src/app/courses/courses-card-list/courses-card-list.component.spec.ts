b57fcf7508df74d97c4cb3197ef8e48ad125336b
KateGordin <kate.gordin@gmail.com>
2023-05-10 16:07:09 +0200

    DOM interaction

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -15,16 +15,18 @@ describe('CoursesCardListComponent', () => {
 
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(waitForAsync( () => {
     TestBed.configureTestingModule({
         imports: [CoursesModule]
     })
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.createComponent(CoursesCardListComponent);
-          component = fixture.componentInstance;
-        });
+    .compileComponents()
+    .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+    });
   }) );
 
 
@@ -38,7 +40,10 @@ describe('CoursesCardListComponent', () => {
 
   it("should display the course list", () => {
 
-    pending();
+    component.courses = setupCourses();
+    const cards = el.queryAll(By.css(".course-card"));
+    expect(cards).toBeTruthy("Could not find cards");
+    expect(cards.length).toBe(12, "Unexpected number of courses");
 
   });
 

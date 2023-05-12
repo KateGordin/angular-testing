9f01fb876e8a35c03b2562fe3f52bbd6a6d8d290
KateGordin <kate.gordin@gmail.com>
2023-05-12 13:16:52 +0200

    Testing the home component

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -21,6 +21,10 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component:HomeComponent;
   let el: DebugElement;
+  let coursesService: any;
+
+  const beginnerCourses = setupCourses()
+        .filter(course => course.category == 'BEGINNER');
 
   beforeEach(waitForAsync(() => {
     const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
@@ -38,6 +42,7 @@ describe('HomeComponent', () => {
           fixture = TestBed.createComponent(HomeComponent);
           component = fixture.componentInstance;
           el = fixture.debugElement;
+          coursesService = TestBed.get(CoursesService);
       });
 
   }));
@@ -51,7 +56,12 @@ describe('HomeComponent', () => {
 
   it("should display only beginner courses", () => {
 
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+
+    fixture.detectChanges();
+      
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found");
 
   });
 
